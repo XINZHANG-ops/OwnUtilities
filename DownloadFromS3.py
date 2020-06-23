98edf94efb9f489126f4d34ff40b07cea07128fe
XINZHANG-ops <47906299+XINZHANG-ops@users.noreply.github.com>
2020-06-23 09:27:30 -0400

    Add files via upload

--- DownloadFromS3.py
+++ DownloadFromS3.py
@@ -1,5 +1,9 @@
 import boto3
+import logging
 from tqdm import tqdm
+import os
+from botocore.exceptions import ClientError
+
 # Let's use Amazon S3
 
 def hook(t):
@@ -7,7 +11,7 @@ def hook(t):
     t.update(bytes_amount)
   return inner
 
-def S3_download(file_name,save_file_name,bucket,show_progress=True):
+def S3_download(file_name,save_file_name,bucket='bonfire-data-team-share',show_progress=True):
     if show_progress:
         s3 = boto3.resource('s3')
         s31 = boto3.client('s3')
@@ -21,4 +25,27 @@ def S3_download(file_name,save_file_name,bucket,show_progress=True):
 
 
 
+def upload_file(file_name,save_file_name=None, bucket='bonfire-data-team-share',show_progress=True):
+    """Upload a file to an S3 bucket
 
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+    # If S3 object_name was not specified, use file_name
+    if save_file_name is None:
+        save_file_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        if show_progress:
+            with tqdm(total=float(os.path.getsize(file_name)), unit='B', unit_scale=True, desc=file_name) as t:
+                s3_client.upload_file(file_name, bucket,save_file_name,Callback=hook(t))
+        else:
+            s3_client.upload_file(file_name, bucket, save_file_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
